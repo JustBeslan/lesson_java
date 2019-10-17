74f990ebcb4d868ef4690dd7392c9ded0c1363f0
Beslannn <IbragimowBeslan@yandex.ru>
2019-10-17 08:25:08 +0300

    lesson_1

--- lesson_java/src/main/java/com/beslan/lab/task4/Child.java
+++ lesson_java/src/main/java/com/beslan/lab/task4/Child.java
@@ -3,6 +3,6 @@ package com.beslan.lab.task4;
 public class Child extends Extends {
     public void print() {
         System.out.println("I'm Child");
-        super.f();
+//        super.f();
     }
 }
--- lesson_java/src/main/java/com/beslan/lab/task4/Fly0.java
+++
@@ -1,4 +0,0 @@
-package com.beslan.lab.task4;
-
-public class Fly0 implements Fly {
-}
